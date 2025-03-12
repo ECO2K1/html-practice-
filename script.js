5b0a4f5704682f3e3d0416b526bcf29f38e9c360
ECO2K1 <eduardobusinessbear@gmail.com>
2025-03-12 14:29:05 -0500

    Wednesday work

---
+++ script.js
@@ -0,0 +1,6 @@
+
+let imgSource = "https://thinkmushrooms.ca/wp-content/uploads/2023/12/The-Ego-Death-and-Spiritual-Awakening-jpeg.webp"
+// function ChangeImg() {
+//     document.getElementById("woke")
+
+// }
